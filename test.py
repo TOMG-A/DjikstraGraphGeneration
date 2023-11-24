302ff479c0a573e4bf9d7b41c7988d175cda0d64
Tom <im.tomtomon@gmail.com>
2023-11-24 13:14:18 +0000

    EdgeList Test

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ print(a)
 print(f"Graph Generation Runtime: {round(end-start,3)*100} ms")
 print(f"Djikstra's Algorithm Runtime: {round(overall_time-end,3)*100} ms")
 print(f"Total Runtime: {round(overall_time-start,3)*100} ms")
+
+
+#### EDGELIST CONVERSION ---- TEST ####
+convert={}
+for a,x in enumerate(graph):
+    convert[a]=[]
+    for y in x.children:
+        convert[a].append(graph._Vertices.index(y[0]))
+print(convert)
